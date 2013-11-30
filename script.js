c87ffbf28c275b7d9c53a65312a07e018f398282
Soufii <abbshr@outlook.com>
2013-11-30 15:33:50 +0800

    告急：localStorage 不能存储对象！

--- script.js
+++ script.js
@@ -64,108 +64,225 @@
         removeItem: function (item) {
             item || return null;
             return database.removeItem(item);
+        },
+        getRandomItem: function (depends) {
+
+        },
+        getCounter: function () {
+            return database.length;
         }
     };
 
     //get frame & btn will be used 
-    var //app = $id('app'),
-        panel = $id('mainpanel'),
-        //viewer_btn = $id('viewdatabase'),
-        //add_btn = $id('add'),
-        //remove_btn = $id('remove'),
-        //start_btn = $id('start'),
-        profile = $id('profile'),
-        //conf = $id('conf'),
-        //plus = $id('plus'),
-        activebox = $id('activebox');
+    var btnGroup = $id('btngrp'),
+        commit = $id('commit'),
+        profile = $id('profile');
 
-    //the elements' id name hash list
+    //the btn' id name hash list
     var actionList = {
-        //'app': ,
-        //'mainpanel': ,
-        'viewdatabase': viewAllWords,
-        'viewwrong': viewWrongWords,
-        'add': addWords,
-        'remove': delWords,
+        'viewall': viewAll,
+        'viewwrong': viewWrong,
         'start': startTest,
-        'commit': commit
-        //'activebox': ,
-        //'profile': ,
-        //'conf': ,
-        //'plus': 
+        'guide': guide,
+        'add': addWord,
+        'remove': removeWord,
+        'wordslist': bindClick,
     };
 
-    //callback of panel's 'Click' event
+    //bind callback for panel's children
+    global.addEventListener('click', callback);
+
+    //callback of app's Click' event
     function callback(e) {
         var targetId = e.target.getAttribute('id');
-
         //router handler by hash list
         actionList[targetId] && actionList[targetId](e.target);
     }
 
-    //router: callback of start_btn's 'Click' event
-    function startTest(target) {}
+    function viewAll(target) {
+        var collection = db.getCollection('words').docs || null;
+        if (!collection) return;
+        var feedback = $id('feedback');
+        feedback.innerHTML = '';
 
-    //router: callback of add_btn's 'Click' event
-    function addWords(target) {
-        
-    }
+        var wordsList = createE('ul');
+        wordsList.setAttribute('id', 'wordslist');
+        wordsList.setAttribute('class', '');
 
-    //router: callback of remove_btn's 'Click' event
-    function delWords(target) {}
-
-    //router: callback of viewer_btn's 'Click' event
-    function viewAllWords(target) {
-        var viewer = $id('activebox');
-        var words_database = db.getCollection('words');
-
-        for (var word in words_database) {
-            var span = createE('span');
-            var contact = createT(word + ', ');
-            span.setAttribute('class', 'word');
-            span.setAttribute('href', 'javascript:void(0)');
-            span.appendChild(contact);
-            viewer.appendChild(span);
+        for (var word in collection) {
+            var item = createE('li');
+            var text = createT(word);
+            item.setAttribute('class', '');
+            item.appendChild(text);
+            wordsList.appendChild(item);
         }
-
-        //bind handler for 'click' event on activebox(viewer)
-        viewer.addEventListener('click', showDetails);
     }
 
-    /* callback of activebox(viewer)'s 'Click' e
-     * this function will check out the e source name & invoke lightBox()
-    */
-    function showDetails(e) {
-        var target = e.target,
-            tarName = target.nodeName.toLowerCase(),
-            text = target.textContent;
+    function viewWrong(target) {
+        var collection = db.getCollection('wrongWords').docs || null;
+        if (!collection) return;
+        var feedback = $id('feedback');
+        feedback.innerHTML = '';
 
-        var collection = db.getCollection('words'),
-            wordDetail = collection.findItem(text);
+        var  wordsList = createE('ul');
+        wordsList.setAttribute('id', 'wordslist');
+        wordsList.setAttribute('class', '');
 
-        (tarName == 'span') && lightBox(target, wordDetail);
+        for (var word in collection) {
+            var item = createE('li');
+            var text = createT(word);
+            item.setAttribute('class', '');
+            item.appendChild(text);
+            wordsList.appendChild(item);
+        }
     }
 
-    function subscribe(target) {}
+    function startTest(target) {
+        var testNum = $id('testnum').value;
+        verify(testNum) && started(testNum)
+    }
 
-    //function getWord(collection) {}
+    function guide(target) {
+        showLightBox();
+    }
 
-    //function copy(collection) {}
+    function comYourAns(target) {
+        var answer = $id('input').value;
+        verify(answer) && committed(answer)
+    }
 
-    /* lightBox
-    *  display a lightBox to show words details
-    *  & auto disappeared it self
-    *  @params target: the target DOM Object of event 
-    *  @params content: content to be shown
-    */
-    function lightBox(target, content) {
+    function addWord(target) {
+        var newword = $id('newword').value,
+            property = $id('property').value,
+            transfer = $id('transfer').value;
+
+        (verify(newword) && verify(property) && verify(transfer)) 
+        && added(newword, property, transfer)   
+    }
+
+    function removeWord(target) {
+        var oldword = $id('oldword').value;
+        verify(oldword) && removed(oldword)
+    }
+
+    function bindEvent(target) {
+        var tarName = target.nodeName.toLowerCase();
+        if (tagName != 'li') return;
+
+        var board = $id('board');
+        text = target.textContent;
+        var wordObj = db.getCollection('words').findItem('text');
+        board.innerHTML = '';
+
+        var h2 = createE('h2'),
+            p1 = createE('p'),
+            p2 = createE('p');
+        var h2_text = createT(wordObj.word),
+            p1_text = createT(wordObj.property),
+            p2_text = createT(wordObj.meaning);
+
+        h2.appendChild(h2_text);
+        p1.appendChild(p1_text);
+        p2.appendChild(p2_text);
+        board.appendChild(h2);
+        board.appendChild(p1);
+        board.appendChild(p2);
+    }
+
+    function started(testNum) {
+        sessions['details'] = [];
+        sessions['testNum'] = testNum;
+        //commit按钮解禁
+        var commit = $id('commit');
+        actionList['commit'] = comYourAns;
+
+        /*var board = $id('board');
+        board.innerHTML = '';*/
+
+        var progressbar = $id('div');
+        var progress = $id('div');
+        progressbar.setAttribute('class', 'progress');
+        progress.setAttribute('id', 'progress');
+        progress.setAttribute('class', 'progress-bar progress-bar-success');
+        progress.setAttribute('role', 'progressbar');
+        progress.setAttribute('aria-valuemin', '0');
+        progress.setAttribute('aria-valuemax', testNum);
+
+        progressbar.appendChild(progress);
+        //board.appendChild(progressbar);
+
+        nextTest(sessions);
+    }
+
+    function committed(answer) {
+        var currentNum = sessions.details.length;
+        var testNum = sessions.testNum;
+        var progress = $id('progress');
+        progress.setAttribute('style', 'width: ' + (currentNum / testNum)*100 + '%');
+        subscribe(answer);
+        nextTest(sessions);
+
+        if (sessions.details.length === sessions.testNum) {
+            var commit = $id('commit');
+            //按钮加禁
+            delete actionList['commit'];
+        }
+    }
+
+    function added(newword, property, transfer) {}
+
+    function removed(oldword) {}
+
+    function nextTest(sessions) {
+
+        var board = $id('board');
+        board.innerHTML = '';
+        var wordObj = db.getCollection('words').getRandomItem(sessions.details);
+        sessions.details.push(wordObj.word);
+
+        var h2 = createE('h2'),
+            p1 = createE('p'),
+            p2 = createE('p');
+        var h2_text = createT(wordObj.word),
+            p1_text = createT(wordObj.property),
+            p2_text = createT(wordObj.meaning);
+
+        h2.appendChild(h2_text);
+        p1.appendChild(p1_text);
+        p2.appendChild(p2_text);
+        board.appendChild(h2);
+        board.appendChild(p1);
+        board.appendChild(p2);
 
     }
 
-    //bind callback for panel's children
-    panel.addEventListener('click', callback);
-    profile.addEventListener('click', callback);
-    activebox.addEventListener('click', callback);
+    function showLightBox() {
+
+    }
+
+    function verify(value) {
+        if (value == 0 && !(/0/i).test(value))
+            return false;
+        return true;
+    }
+
+    function subscribe(answer) {
+        var feedback = $id('feedback');
+        feedback.innerHTML = '';
+        var alert = createE('div');
+        var text = createT('Result:');
+
+        var rightAns = sessions.details[0];
+        if (answer == rightAns) {
+            alert.setAttribute('class', 'alert alert-success');
+            text.textContent = 'Perfact! Answer Right!'
+        } else {
+            alert.setAttribute('class', 'alert alert-danger');
+            text.textContent = 'Ooh~, Sorry u r Wrong';
+        }
+        alert.appendChild(text);
+        feedback.appendChild(alert);
+    }
 
     return console.log("Initialization successful~");
 })(this);
